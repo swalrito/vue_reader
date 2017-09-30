f06ddfd77071a074213b3e477561abef758e01a0
彭定林 <pengdinglin@pengdinglindeMacBook-Pro.local>
2017-09-30 15:13:24 +0800

    2017.9.30 完成rank页面布局

--- src/components/page/rank/rank.vue
+++ src/components/page/rank/rank.vue
@@ -1,6 +1,23 @@
 <template>
 	<div class="rank">
-		<top name='排行'></top>
+		<div class="man_rank rank_content">
+			男生
+			<ul>
+				<li><router-link to=''><img src="static/img/下载.svg"> 一周热门榜</router-link></li>
+				<li><router-link to=''><img src="static/img/下载.svg"> 当月热门榜</router-link></li>
+				<li><router-link to=''><img src="static/img/下载.svg"> 完结热门榜</router-link></li>
+				<li><router-link to=''><img src="static/img/下载.svg"> 收藏热门榜</router-link></li>
+			</ul>
+		</div>
+		<div class="woman_rank rank_content">
+			女生
+			<ul>
+				<li><router-link to=''><img src="static/img/下载.svg"> 一周热门榜</router-link></li>
+				<li><router-link to=''><img src="static/img/下载.svg"> 当月热门榜</router-link></li>
+				<li><router-link to=''><img src="static/img/下载.svg"> 完结热门榜</router-link></li>
+				<li><router-link to=''><img src="static/img/下载.svg"> 收藏热门榜</router-link></li>
+			</ul>
+		</div>
 	</div>
 </template>
 
@@ -14,5 +31,29 @@ import top from '../../public/top.vue'
 </script>
 
 <style type="text/css">
-	
+	.rank_content{
+		margin-top: 0.33rem;
+		padding: 0.27rem;
+		font-size: 24px;
+		text-align: left;
+	}
+	.rank_content ul{
+		margin-top: 0.27rem;
+	}
+	.rank_content ul li{
+		border-bottom: 1px solid #E0E0E0;
+		padding-left: 0.43rem;
+		font-size: 20px;
+		margin-bottom: 0.27rem;
+	}
+	.rank_content ul li a{
+		color: #000;
+		outline: none;
+	}
+	.rank_content ul li img{
+		width: 24px;
+		height: 24px;
+		display: inline-block;
+	}
+
 </style>
