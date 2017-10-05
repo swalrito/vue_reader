5d5a41f4f659cb88da02bdffcd9cfa375e1c1e07
彭定林 <pengdinglin@pengdinglindeMacBook-Pro.local>
2017-10-05 22:04:17 +0800

    2017.10.15 将返回按钮做成组件。

---
+++ src/components/page/sort/sort_details.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="sort_details">
+		<top name='分类详情'></top>
+		<return></return>
+		<nav-bar></nav-bar>
+		<nav-bar></nav-bar>
+		<book-detail></book-detail>
+	</div>
+</template>
+
+<script type="text/javascript">
+	import return from '../../public/return.vue'
+	import top from '../../public/top.vue'
+	import navBar from '../../public/details_navbar.vue'
+	import bookDetail from '../../public/book_detail.vue'
+	export default{
+		components:{
+			top,
+			navBar,
+			bookDetail,
+			return
+		}
+	}
+</script>
+
+<style type="text/css">
+	.sort_details{
+		height: 100%;
+		width: 100%;
+		overflow-y: scroll;
+		background-color: #E6E6E6;
+	}
+</style>
---
+++ src/components/public/return.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="return">
+		<span><icon name='return' class='return_icon' scale='2.5'></icon>返回</span>
+	</div>
+</template>
+
+<script type="text/javascript">
+	
+</script>
+
+<style type="text/css">
+	.return{
+		position: absolute;
+		top: 0.2rem;
+		left: 0.2rem;
+		height: 0.6rem;
+	}
+	.return span{
+		font-size: 16px;
+		height: 0.6rem;
+		line-height: 0.6rem;
+	}
+	.return span .return_icon{
+		vertical-align: text-bottom;
+	}
+</style>
