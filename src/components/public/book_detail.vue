882db1fbc41919266ac1219431af4f854ceae770
彭定林 <1979945716@qq.com>
2017-10-06 22:17:55 +0800

    2017.10.6 完成书本列表背景色。

--- src/components/public/book_detail.vue
+++ src/components/public/book_detail.vue
@@ -10,6 +10,78 @@
 					<p class="book_status">20.5万人气		|	85.9%读者留存率</p>
 				</div>
 			</li>
+			<li>
+				<img src="../../assets/book.jpg">
+				<div class="book_info">
+					<p class="book_name">一念永恒</p>
+					<p class="book_author">耳根</p>
+					<p class="book_intro">一念成沧海，一念化桑田。一念斩千魔，一念诛万仙。唯我念……永恒这是耳根继《仙逆》《求魔》《我欲封天》后，创作的第四部长篇小说《一念永恒》</p>
+					<p class="book_status">20.5万人气		|	85.9%读者留存率</p>
+				</div>
+			</li>
+			<li>
+				<img src="../../assets/book.jpg">
+				<div class="book_info">
+					<p class="book_name">一念永恒</p>
+					<p class="book_author">耳根</p>
+					<p class="book_intro">一念成沧海，一念化桑田。一念斩千魔，一念诛万仙。唯我念……永恒这是耳根继《仙逆》《求魔》《我欲封天》后，创作的第四部长篇小说《一念永恒》</p>
+					<p class="book_status">20.5万人气		|	85.9%读者留存率</p>
+				</div>
+			</li>
+			<li>
+				<img src="../../assets/book.jpg">
+				<div class="book_info">
+					<p class="book_name">一念永恒</p>
+					<p class="book_author">耳根</p>
+					<p class="book_intro">一念成沧海，一念化桑田。一念斩千魔，一念诛万仙。唯我念……永恒这是耳根继《仙逆》《求魔》《我欲封天》后，创作的第四部长篇小说《一念永恒》</p>
+					<p class="book_status">20.5万人气		|	85.9%读者留存率</p>
+				</div>
+			</li>
+			<li>
+				<img src="../../assets/book.jpg">
+				<div class="book_info">
+					<p class="book_name">一念永恒</p>
+					<p class="book_author">耳根</p>
+					<p class="book_intro">一念成沧海，一念化桑田。一念斩千魔，一念诛万仙。唯我念……永恒这是耳根继《仙逆》《求魔》《我欲封天》后，创作的第四部长篇小说《一念永恒》</p>
+					<p class="book_status">20.5万人气		|	85.9%读者留存率</p>
+				</div>
+			</li>
+			<li>
+				<img src="../../assets/book.jpg">
+				<div class="book_info">
+					<p class="book_name">一念永恒</p>
+					<p class="book_author">耳根</p>
+					<p class="book_intro">一念成沧海，一念化桑田。一念斩千魔，一念诛万仙。唯我念……永恒这是耳根继《仙逆》《求魔》《我欲封天》后，创作的第四部长篇小说《一念永恒》</p>
+					<p class="book_status">20.5万人气		|	85.9%读者留存率</p>
+				</div>
+			</li>
+			<li>
+				<img src="../../assets/book.jpg">
+				<div class="book_info">
+					<p class="book_name">一念永恒</p>
+					<p class="book_author">耳根</p>
+					<p class="book_intro">一念成沧海，一念化桑田。一念斩千魔，一念诛万仙。唯我念……永恒这是耳根继《仙逆》《求魔》《我欲封天》后，创作的第四部长篇小说《一念永恒》</p>
+					<p class="book_status">20.5万人气		|	85.9%读者留存率</p>
+				</div>
+			</li>
+			<li>
+				<img src="../../assets/book.jpg">
+				<div class="book_info">
+					<p class="book_name">一念永恒</p>
+					<p class="book_author">耳根</p>
+					<p class="book_intro">一念成沧海，一念化桑田。一念斩千魔，一念诛万仙。唯我念……永恒这是耳根继《仙逆》《求魔》《我欲封天》后，创作的第四部长篇小说《一念永恒》</p>
+					<p class="book_status">20.5万人气		|	85.9%读者留存率</p>
+				</div>
+			</li>
+			<li>
+				<img src="../../assets/book.jpg">
+				<div class="book_info">
+					<p class="book_name">一念永恒</p>
+					<p class="book_author">耳根</p>
+					<p class="book_intro">一念成沧海，一念化桑田。一念斩千魔，一念诛万仙。唯我念……永恒这是耳根继《仙逆》《求魔》《我欲封天》后，创作的第四部长篇小说《一念永恒》</p>
+					<p class="book_status">20.5万人气		|	85.9%读者留存率</p>
+				</div>
+			</li>
 		</ul>
 	</div>
 </template>
@@ -21,6 +93,9 @@
 <style type="text/css">
 	.book_list{
 		width: 100%;
+		overflow-y: scroll;
+		background-color: #E6E6E6;
+
 	}
 	.book_list li{
 		width: 100%;
